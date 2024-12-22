e75fe2bc39e7773aeec18856e919c3e2222f93ae
GabiB9 <gabidenbeisembayuly14@gmail.com>
2024-12-23 03:22:06 +0500

    Update Database.java

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -21,10 +21,7 @@ import java.util.Vector;
  * <!--  end-user-doc  -->
  */
 public class Database implements Serializable{
-    /**
-     * <!-- begin-user-doc -->
-     * <!--  end-user-doc  -->
-     */
+    
     static final String PATH = "./project-x/src/database/data/";
     private Vector<Course> courses;
 
